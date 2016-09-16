c8fcf6967da2a592befd52dc16a7a098b8d71ee9
joyharr162 <joyharr162@ed.amdsb.ca>
2016-09-16 13:02:24 -0400

    Add project files.

---
+++ Formative Game/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Formative_Game
+{
+    partial class formative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Trajan Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(71, 82);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(148, 43);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            this.startButton.MouseHover += new System.EventHandler(this.startButton_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "       Game Starting in:";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "3";
+            this.label2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.label3.Location = new System.Drawing.Point(133, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2";
+            this.label3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(197, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "1";
+            // 
+            // formative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Name = "formative";
+            this.Opacity = 0.8D;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.RichTextBox label1;
+        private System.Windows.Forms.TextBox label2;
+        private System.Windows.Forms.TextBox label3;
+        private System.Windows.Forms.TextBox label4;
+    }
+}
+
---
+++ Formative Game/Form1.cs
@@ -0,0 +1,107 @@
+﻿/// Created by Joy Harris
+/// on September 8 2016
+/// 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace Formative_Game
+{
+    public partial class formative : Form
+    {
+        public formative()
+        {
+            InitializeComponent();
+        }
+
+        /*when the button in clicked, the button disappears, and a message appears*/
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.WhiteSmoke;//countdown to game starting
+            Refresh();
+            Thread.Sleep(2000);
+            label1.BackColor = Color.Black;
+
+            startButton.BackColor = Color.Black;
+
+            label2.BackColor = Color.WhiteSmoke;
+            Refresh();
+            Thread.Sleep(1000);
+            label2.BackColor = Color.Black;
+
+            label3.BackColor = Color.WhiteSmoke;
+            Refresh();
+            Thread.Sleep(1000);
+            label3.BackColor = Color.Black;
+
+            label4.BackColor = Color.WhiteSmoke;
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.Black;
+
+            Refresh();
+            Thread.Sleep(1000);
+            label4.BackColor = Color.Black;
+
+            startButton.BackColor = Color.ForestGreen;//changes screen to green, clears labels
+            label1.Dispose();
+            label2.Dispose();
+            label3.Dispose();
+            label4.Dispose();
+            startButton.Dispose();
+            BackColor = Color.ForestGreen;
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.alert);//plays sound
+            player.Play();
+
+            Refresh();
+            Thread.Sleep(1000);//wait one second
+
+            Graphics formGraphics = this.CreateGraphics();//pens and brushes
+            Pen drawPen = new Pen(Color.Black, 10);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            Font drawFont = new Font("Arial",8, FontStyle.Bold);
+            Font secondFont = new Font("Copperplate Gothic Bold", 44, FontStyle.Bold);
+            SolidBrush secondBrush = new SolidBrush(Color.BlueViolet);
+            SolidBrush thirdBrush = new SolidBrush(Color.Orange);
+
+            /*formGraphics.FillRectangle(drawBrush, 60, 80, 50, 100);//draw logo
+            formGraphics.TranslateTransform(125,80);//draw and rotate string
+            formGraphics.RotateTransform(90);
+            formGraphics.DrawString("Central Rams Arcade", drawFont, drawBrush, new Rectangle());
+            formGraphics.ResetTransform();
+            formGraphics.FillEllipse(thirdBrush, 82, 100, 10, 20);
+            formGraphics.DrawString("C", secondFont, secondBrush, 55, 75);*/
+        
+            Thread.Sleep(3000);//wait three seconds
+
+            formGraphics.Clear(Color.Black);//clear the screen
+
+            formGraphics.FillRectangle(thirdBrush, 20, 20, 160, 15);
+            formGraphics.FillRectangle(thirdBrush, 20, 80, 100, 15);
+            formGraphics.FillRectangle(thirdBrush, 180, 20, 15, 250);
+            formGraphics.FillRectangle(thirdBrush, 120, 80, 15, 200);
+            formGraphics.FillPie(secondBrush, 20, 50, 30, 30, 30, 300);//draw pacman
+            Thread.Sleep(1000);
+
+            formGraphics.FillEllipse(drawBrush, 20, 50, 30, 30);
+            formGraphics.FillEllipse(secondBrush, 100, 50, 30, 30);
+            Thread.Sleep(1000);
+        }
+
+        private void startButton_MouseHover(object sender, EventArgs e)/*When you hover over 
+            the start button, its color changes*/
+        {
+            
+            startButton.BackColor = Color.WhiteSmoke;
+        
+        }
+    }
+}
